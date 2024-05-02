6cfbd3d2a068a4612331f2e1a6714700f68e353a
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-05-02 13:19:52 +0700

    CHAPTER 4 SUB CHAPTER 1

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by MacBook Pro on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
